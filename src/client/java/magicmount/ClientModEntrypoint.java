a51447defee6ac3e682cf2b787e6cefc80768e69
Tilmann <tilmann.lorenz@gmx.de>
2024-01-29 21:38:14 +0100

    Minecraft mod works and is working heyo

---
+++ src/client/java/magicmount/ClientModEntrypoint.java
@@ -0,0 +1,10 @@
+package magicmount;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientModEntrypoint implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/magicmount/mixin/ServerMixin.java
@@ -0,0 +1,9 @@
+package magicmount.mixin;
+
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
+public class ServerMixin {
+}

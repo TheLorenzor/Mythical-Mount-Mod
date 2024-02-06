fea14304e3fc0071891af8501903b7de1a03d034
Tilmann <tilmann.lorenz@gmx.de>
2024-02-06 23:53:06 +0100

    Starting with dragon implementation functionality wise

--- src/main/java/magicmount/creature/FlyingCreature.java
+++ src/main/java/magicmount/creature/FlyingCreature.java
@@ -1,10 +1,11 @@
 package magicmount.creature;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.world.World;
 
-public class FlyingCreature extends PathAwareEntity {
+public class FlyingCreature extends MobEntity {
     public FlyingCreature(EntityType<? extends PathAwareEntity> entityType, World world) {
         super(entityType, world);
     }

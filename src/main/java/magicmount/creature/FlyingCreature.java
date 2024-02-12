dee051c33c2fc25755b9d2a70596c61446461cb6
Tilmann <tilmann.lorenz@gmx.de>
2024-02-13 00:17:56 +0100

    WE starting with the movement

--- src/main/java/magicmount/creature/FlyingCreature.java
+++ src/main/java/magicmount/creature/FlyingCreature.java
@@ -4,11 +4,21 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 abstract public class FlyingCreature extends FlyingEntity{
 
     public FlyingCreature(EntityType<? extends FlyingEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        Vec3d vec3d = new Vec3d(1.0f,0,0);
+        if (this.getVelocity().x != 1.0f) {
+            this.setVelocity(vec3d);
+        }
     }
 }
